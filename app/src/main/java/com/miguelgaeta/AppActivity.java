ae11843330c0ae898b937e058fdec4c1a0e00d64
mrkcsc <miguel@miguelgaeta.com>
2015-12-15 16:14:58 -0800

    Initial commit of project.

---
+++ app/src/main/java/com/miguelgaeta/AppActivity.java
@@ -0,0 +1,12 @@
+package com.miguelgaeta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
